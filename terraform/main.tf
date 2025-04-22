57c2f5e1012e4c2f07e0bff8b52f56b86a513a27
pavi2001 <74369749+amie2001@users.noreply.github.com>
2025-04-22 14:20:22 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,41 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_security_group" "strapi_sg_task4" {
+  name        = "strapi-sg_task4"
+  description = "Security group for Strapi EC2 instance"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow SSH from anywhere (can be restricted for security)
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP access (mapped to Strapi's 1337)
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "strapi_task4" {
-  ami                    = "ami-0e35ddab05955cf57"  # Ubuntu AMI (Mumbai region)
+  ami                    = "ami-0e35ddab05955cf57"  # Amazon Linux 2 AMI (Mumbai region)
   instance_type          = "t2.medium"
   key_name               = "strapi-using-docker"     # Your EC2 key pair name
   associate_public_ip_address = true
   security_groups             = [aws_security_group.strapi_sg_task4.name]
 
-  user_data = <<-EOF
+ user_data = <<-EOF
     #!/bin/bash
     apt-get update -y
     apt-get install -y docker.io unzip
