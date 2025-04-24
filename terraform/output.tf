e0e4091d8454e07d472fa72a8a72e88e94d5f653
pavi2001 <74369749+amie2001@users.noreply.github.com>
2025-04-24 11:34:17 +0530

    Create output.tf

---
+++ terraform/output.tf
@@ -0,0 +1,4 @@
+output "alb_url" {
+  description = "URL of the ALB"
+  value       = aws_lb.main.dns_name
+}                                                                                                                                                                                                         
