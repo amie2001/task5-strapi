63374f0b75600f65a5b79afa6837f7c86135e4bf
pavi2001 <74369749+amie2001@users.noreply.github.com>
2025-04-29 14:15:21 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,13 +7,3 @@ variable "container_port" {
   description = "Port that Strapi container listens on"
   default     = 1337  # Strapi default port
 }
-
-variable "ecr_repo_name" {
-  description = "Name of the ECR repository"
-  default     = "strapi-app-repo"
-}
-
-variable "project_name" {
-  description = "Name of the project"
-  default     = "strapi-app"
-}             
